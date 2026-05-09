808960704961ed247aa040d26f390379b7cec7b0
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2026-05-09 10:25:53 +0530

    bug fixes

--- app/apis/payment/webhook/route.ts
+++ app/apis/payment/webhook/route.ts
@@ -6,17 +6,45 @@ import OddoAxios from "@/src/libs/Oddo";
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
+  const { searchParams } = new URL(req.url);
+
+  const paramsObject = Object.fromEntries(searchParams.entries());
+
+  const {
+    homeCategory,
+    location,
+    services,
+    coordinates,
+    city,
+    childCategory,
+    childCategoryName,
+    product,
+    price,
+    productName,
+    attribute,
+    voucher,
+    voucherPrice,
+    equipmentName,
+    equipmentId,
+    customerId,
+    customerName,
+    customerEmail,
+    customerPhone,
+    salesOderId,
+  } = paramsObject;
+
   const {
     account_number,
     amount,
-    card_brand,
-    card_last4,
     completed_at,
     currency,
     customer_name,
     ecocash_reference,
     event,
+    payment_id,
     payment_method,
+    payment_reference,
+    phone,
     provider_reference,
     reference,
     status,
@@ -24,19 +52,18 @@ export async function POST(req: NextRequest) {
     transaction_id,
   } = body;
 
-  // if (status === "completed") {
-  //   const invoice = await OddoAxios.post(
-  //     "/json/2/sale.order/_create_invoices",
-  //     {
-  //       ids: [transaction_id],
-  //     },
-  //   );
-  // }
+  if (status === "completed" && payment_id) {
+    const invoice = await OddoAxios.post(
+      "/json/2/sale.order/_create_invoices",
+      {
+        ids: [salesOderId],
+      },
+    ).then((res) => res.data);
+
+    console.log("Invoice created successfully:", invoice);
+  }
 
   // ✅ get query params
-  const { searchParams } = new URL(req.url);
-
-  const paramsObject = Object.fromEntries(searchParams.entries());
 
   console.log(body, paramsObject);
 

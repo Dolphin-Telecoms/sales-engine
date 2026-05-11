47feda123476f0ea0c2dad0f3ddfdd820fcd829b
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2026-05-11 20:07:17 +0530

    bug fixed

--- app/apis/payment/webhook/route.ts
+++ app/apis/payment/webhook/route.ts
@@ -19,73 +19,106 @@ export async function POST(req: NextRequest) {
     console.log(body, paramsObject);
 
     if (status === "completed" && payment_id) {
-      const confirmsaleOrder = await OddoAxios.post(
-        "/json/2/sale.order/action_confirm",
-        { ids: [salesOderId] },
-      );
-      console.log("confirmsaleOrder :: ", confirmsaleOrder);
-      if (confirmsaleOrder) {
-        const wizard = await OddoAxios.post(
-          "/json/2/sale.advance.payment.inv/create",
-          {
-            vals_list: [
-              {
-                advance_payment_method: "delivered",
-                sale_order_ids: [[6, 0, [salesOderId]]],
-              },
-            ],
-          },
+      try {
+        const confirmsaleOrder = await OddoAxios.post(
+          "/json/2/sale.order/action_confirm",
+          { ids: [parseInt(salesOderId)] },
         ).then((res) => res.data);
-        console.log("wizard created successfully:", wizard);
-        if (wizard && Array.isArray(wizard) && wizard.length > 0) {
-          const invoice = await OddoAxios.post(
-            "/json/2/sale.advance.payment.inv/create_invoices",
-            { ids: wizard[0] },
-          ).then((res) => res.data);
-          console.log("invoice created successfully:", invoice);
-          const { id, res_id } = invoice;
-          if (id && res_id) {
-            const action = await OddoAxios.post(
-              "/json/2/account.move/action_post",
-              { ids: [res_id] },
-            ).then((res) => res.data);
-            console.log("action post successfully:", action);
-
-            const unreconciled = await OddoAxios.post(
-              "/json/2/account.move.line/search_read",
+        console.log("confirmsaleOrder :: ", confirmsaleOrder);
+        if (confirmsaleOrder) {
+          try {
+            const wizard = await OddoAxios.post(
+              "/json/2/sale.advance.payment.inv/create",
               {
-                domain: [
-                  ["payment_id", "=", payment_id],
-                  [
-                    "account_type",
-                    "in",
-                    ["asset_receivable", "liability_payable"],
-                  ],
-                  ["reconciled", "=", false],
+                vals_list: [
+                  {
+                    advance_payment_method: "delivered",
+                    sale_order_ids: [[6, 0, [parseInt(salesOderId)]]],
+                  },
                 ],
-                fields: ["id", "name", "amount_residual"],
               },
             ).then((res) => res.data);
+            console.log("wizard created successfully:", wizard);
+            if (wizard && Array.isArray(wizard) && wizard.length > 0) {
+              try {
+                const invoice = await OddoAxios.post(
+                  "/json/2/sale.advance.payment.inv/create_invoices",
+                  { ids: wizard[0] },
+                ).then((res) => res.data);
+                console.log("invoice created successfully:", invoice);
+                const { id, res_id } = invoice;
+                if (id && res_id) {
+                  try {
+                    const action = await OddoAxios.post(
+                      "/json/2/account.move/action_post",
+                      { ids: [res_id] },
+                    ).then((res) => res.data);
+                    console.log("action post successfully:", action);
 
-            if (
-              unreconciled &&
-              Array.isArray(unreconciled) &&
-              unreconciled.length > 0
-            ) {
-              const reconcile = await OddoAxios.post(
-                "/json/2/account.move/js_assign_outstanding_line",
-                {
-                  ids: [res_id],
-                  line_id: unreconciled[0].id,
-                },
-              ).then((res) => res.data);
+                    const unreconciled = await OddoAxios.post(
+                      "/json/2/account.move.line/search_read",
+                      {
+                        domain: [
+                          ["payment_id", "=", payment_id],
+                          [
+                            "account_type",
+                            "in",
+                            ["asset_receivable", "liability_payable"],
+                          ],
+                          ["reconciled", "=", false],
+                        ],
+                        fields: ["id", "name", "amount_residual"],
+                      },
+                    ).then((res) => res.data);
+                    console.log("unreconciled lines:", unreconciled);
+                    if (
+                      unreconciled &&
+                      Array.isArray(unreconciled) &&
+                      unreconciled.length > 0
+                    ) {
+                      try {
+                        const reconcile = await OddoAxios.post(
+                          "/json/2/account.move/js_assign_outstanding_line",
+                          {
+                            ids: [res_id],
+                            line_id: unreconciled[0]?.id,
+                          },
+                        ).then((res) => res.data);
 
-              if (reconcile === null) {
-                return NextResponse.json({ received: true });
+                        if (reconcile === null) {
+                          return NextResponse.json({ received: true, reconcile: true });
+                        }
+                      } catch (error) {
+                        console.error(
+                          "Error in fetching reconcile lines:",
+                          error,
+                        );
+                        return NextResponse.json({ reconcile: false });
+                      }
+                    } else {
+                      return NextResponse.json({ unreconciled: false });
+                    }
+                  } catch (error) {
+                    console.error("Error in posting invoice:", error);
+                    return NextResponse.json({
+                      action: false,
+                      unreconciled: false,
+                    });
+                  }
+                }
+              } catch (error) {
+                console.error("Error in invoice creation:", error);
+                return NextResponse.json({ invoice: false });
               }
             }
+          } catch (error) {
+            console.error("Error in wizard webhook processing:", error);
+            return NextResponse.json({ wizard: false });
           }
         }
+      } catch (error) {
+        console.error("Error in confirmsaleOrder webhook processing:", error);
+        return NextResponse.json({ confirmsaleOrder: false });
       }
     }
 
